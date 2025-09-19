b4876136c4719dafdd29270b9e306f4733e3f599
HuynhAnhDuy <huynhduyds@gmail.com>
2025-09-19 13:49:32 +0700

    update

--- preprocessing/chuyentxtsangcsv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Đọc file txt (ở đây giả sử phân cách bằng tab)
-df = pd.read_csv("NPASSv2.0.txt", sep="\t")
-
-# Xuất sang CSV
-df.to_csv("NPASSv2.0.csv", index=False)
-
-print("Đã lưu thành công data.csv")
